fecde6de83d898ba08dcc049003e7d24f4699871
José Daniel Solano Garcia <usuario3revolt@gmail.com>
2025-11-07 13:36:55 -0500

    config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: path.resolve(__dirname, '../public'),
+  publicDir: path.resolve(__dirname, '../Public'),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
